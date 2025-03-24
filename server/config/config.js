d76dd8d73f6781f419a6c088dd86a400a3773310
JuanSebastianBG <juansebastianbernalgamboa@gmail.com>
2025-03-24 16:57:41 -0500

    Server: config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "192.168.0.9",  // Solo la IP o dominio, sin puerto
+  host: "127.0.0.1",  // Solo la IP o dominio, sin puerto
   port: 3306,              // Puerto de MySQL (por defecto es 3306)
   user: "root",
   password: "",            // Asegúrate de que coincida con tu contraseña de MySQL
